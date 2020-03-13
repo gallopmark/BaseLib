edcb1715f92bcd03800a6f52322631a0edd65e4a
tthwy <i23xiaoyan@qq.com>
2020-03-13 16:17:46 +0800

    v1.0.4

--- library/src/main/java/pony/xcode/system/ToastCompat.java
+++ library/src/main/java/pony/xcode/system/ToastCompat.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,39 +13,9 @@ import androidx.annotation.DrawableRes;
 import androidx.annotation.StringRes;
 import androidx.core.view.ViewCompat;
 
-import java.lang.reflect.Field;
-
 import pony.xcode.base.R;
 
 public class ToastCompat {
-
-    /*自定义toast动画*/
-    class AnimationToast extends Toast {
-
-        @SuppressWarnings("JavaReflectionMemberAccess")
-        AnimationToast(Context context, int windowAnimations) {
-            super(context);
-            Class<Toast> clazz = Toast.class;
-            try {
-                Field field = clazz.getDeclaredField("mTN");
-                field.setAccessible(true);
-                Object obj = field.get(this);
-                if (obj != null) {
-                    // 取消掉各个系统的默认toast弹出动画
-                    Field fieldParam = obj.getClass().getDeclaredField("mParams");
-                    field.setAccessible(true);
-                    Object params = fieldParam.get(obj);
-                    if (params == null) return;
-                    if (params instanceof WindowManager.LayoutParams) {
-                        WindowManager.LayoutParams wl = (WindowManager.LayoutParams) params;
-                        wl.windowAnimations = windowAnimations;
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     private Context mContext;
     private Toast mToast;
@@ -55,7 +24,6 @@ public class ToastCompat {
     private int mDuration = Toast.LENGTH_SHORT;
     private int mGravity = -1;
     private float mHorizontalMargin = -1, mVerticalMargin = -1;
-    private int mWindowAnimations = R.style.ToastCompatStyle; //默认toast动画 淡入淡出
 
     private ToastCompat(Context context) {
         this.mContext = context;
@@ -96,12 +64,6 @@ public class ToastCompat {
         /*构造方法带toast文本*/
         FluentInitializer(CharSequence text) {
             mToastTextView.setText(text);
-        }
-
-        /*设置toast动画*/
-        public FluentInitializer setWindowAnimations(int windowAnimations) {
-            mWindowAnimations = windowAnimations;
-            return this;
         }
 
         /*设置toast文本边距*/
@@ -172,7 +134,7 @@ public class ToastCompat {
 
     /*先判断toast知否为null，不为null则取消toast并设为null*/
     public void show() {
-        mToast = new AnimationToast(mContext, mWindowAnimations);  /*new Toast创建toast 没有设置view会报错*/
+        mToast = new Toast(mContext);  /*new Toast创建toast 没有设置view会报错*/
         mToast.setView(mContentView);
         mToast.setDuration(mDuration);  //默认toast时间
         if (mGravity != -1) {
